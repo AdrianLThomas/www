4f15aa5aa14a4830a7a8e9d94837d9c94050f2e5
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2021-08-26 21:45:41 +0100

    brain-dump

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -24,14 +24,22 @@ const BlogIndex = ({ data, location }) => {
                 itemScope
                 itemType="http://schema.org/Article"
               >
-                <Link href="/blog">Blog</Link>
-                
-                TODO
-                - full width header with links to social, blog page, github, etc.
-                - recent posts
-                - footer
-                
-
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
               </article>
             </li>
           )
