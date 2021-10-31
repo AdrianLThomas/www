989f71990c8969e75acfd6c3a6c93bae55d863e8
Adrian L Thomas <adrian.thomas@gmail.com>
2021-10-31 10:06:49 +0000

    seo

--- cypress/integration/seo.spec.js
+++ cypress/integration/seo.spec.js
@@ -7,6 +7,13 @@ describe("SEO Checks", () => {
     cy.request(`/robots.txt`).its("body").should("include", "Allow: /")
   })
 
+  it("rss.xml", () => {
+    cy.request(`/rss.xml`)
+      .its("body")
+      .should("include", '<rss version="2.0">')
+      .and("include", "<title>The Beginning</title>")
+  })
+
   it("sitemap.xml", () => {
     cy.request(`/sitemap/sitemap-index.xml`)
       .its("body")
